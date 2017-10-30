77cd22c37f47c80e19634b2a927f4dba02f15c73
Guillaume PROQUIN <guillaume@fat.sh>
2017-10-30 21:15:50 +0800

    WareCloud API v0.3.3
    Logout test

---
+++ tests/Feature/LogoutTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\User;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class LogoutTest extends TestCase
+{
+    public function testUserIsLoggedOutProperly()
+    {
+        $user = factory(User::class)->create();
+        $token = $user->generateToken();
+        $headers = ['Authorization' => "Bearer $token"];
+
+        $this->json('GET', '/api/software', [], $headers)->assertStatus(200);
+        $this->json('POST', '/api/user/logout', [], $headers)->assertStatus(200);
+
+        $user = User::find($user->id);
+
+        $this->assertEquals(null, $user->api_token);
+    }
+
+    public function testUserWithNullToken()
+    {
+        // Simulating login
+        $user = factory(User::class)->create();
+        $token = $user->generateToken();
+        $headers = ['Authorization' => "Bearer $token"];
+
+        // Simulating logout
+        $user->api_token = null;
+        $user->save();
+
+        $this->json('GET', '/api/software', [], $headers)->assertStatus(401);
+    }
+}
