8a33d1962508be6a1c9ba121edc6c9e258bcf142
Guillaume PROQUIN <guillaume@fat.sh>
2017-10-30 21:06:34 +0800

    WareCloud API v0.3.2
    Register test

---
+++ tests/Feature/RegisterTest.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class RegisterTest extends TestCase
+{
+    public function testsRegistersSuccessfully()
+    {
+        $payload = [
+            'login' => 'WareCloudTest',
+            'password' => 'WareCloudTest123',
+            'password_confirmation' => 'WareCloudTest123'
+        ];
+
+        $this->json('POST', '/api/user/register', $payload)
+            ->assertStatus(201)
+            ->assertJsonStructure([
+                'data' => [
+                    'id',
+                    'login',
+                    'created_at',
+                    'updated_at',
+                    'api_token'
+                ]
+            ]);
+    }
+
+    public function testsRequiresPasswordEmailAndName()
+    {
+        $this->json('POST', '/api/user/register')
+            ->assertStatus(422)
+            ->assertJson([
+                'message' => 'The given data was invalid.',
+                'errors' => [
+                    'login' => ['The login field is required.'],
+                    'password' => ['The password field is required.']
+                ]
+            ]);
+    }
+
+    public function testsRequirePasswordConfirmation()
+    {
+        $payload = [
+            'login' => 'WareCloudTest',
+            'password' => 'WareCloudTestl123'
+        ];
+
+        $this->json('POST', '/api/user/register', $payload)
+            ->assertStatus(422)
+            ->assertJson([
+                'message' => 'The given data was invalid.',
+                'errors' => [
+                    'password' => ['The password confirmation does not match.']
+                ]
+            ]);
+    }
+}
