1fbcf70c39e78bdfd2a2cc684877d032e1c1f119
Guillaume PROQUIN <guillaume.proquin@epitech.eu>
2018-06-06 13:24:58 +0200

    Refactor login controller Add login throttling

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Http\Request;
-use Auth;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
@@ -29,6 +29,10 @@ class LoginController extends Controller
      */
     protected $redirectTo = '/home';
 
+    protected $maxAttempts;
+
+    protected $decayMinutes;
+
     /**
      * Create a new controller instance.
      *
@@ -36,29 +40,27 @@ class LoginController extends Controller
      */
     public function __construct()
     {
+        $this->maxAttempts = env('LOGIN_MAX_ATTEMPTS', 5);
+        $this->decayMinutes = env('LOGIN_DECAY_MINUTES', 15);
         $this->middleware('guest')->except('logout');
     }
 
+    /**
+     * Get the login username to be used by the controller.
+     *
+     * @return string
+     */
     public function username()
     {
         return 'login';
     }
 
-    public function login(Request $request)
-    {
-        $this->validateLogin($request);
-
-        if ($this->attemptLogin($request))
-        {
-            $user = $this->guard()->user();
-            $user->generateToken();
-
-            return ['data' => $user];
-        }
-
-        return $this->sendFailedLoginResponse($request);
-    }
-
+    /**
+     * Log the user out of the application.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
     public function logout(Request $request)
     {
         $user = Auth::guard('api')->user();
@@ -72,6 +74,41 @@ class LoginController extends Controller
         return ['data' => 'User logged out.'];
     }
 
+    /**
+     * Send the response after the user was authenticated.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    protected function sendLoginResponse(Request $request)
+    {
+        $this->clearLoginAttempts($request);
+
+        return $this->authenticated($request, $this->guard()->user());
+    }
+
+    /**
+     * The user has been authenticated.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  mixed  $user
+     * @return mixed
+     */
+    protected function authenticated(Request $request, $user)
+    {
+        $user->generateToken();
+
+        return ['data' => $user];
+    }
+
+    /**
+     * Get the failed login response instance.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     *
+     * @throws \Illuminate\Validation\ValidationException
+     */
     protected function sendFailedLoginResponse(Request $request)
     {
         return response()->json([
